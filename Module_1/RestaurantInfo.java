124c69de29892ca01c92398fce7197129eec9764
sfranchinicode <sam.franchini92@gmail.com>
2026-05-05 12:09:16 -0700

    Adding Modules 1 & 3

---
+++ Module_1/RestaurantInfo.java
@@ -0,0 +1,12 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+
+        String restaurantName = "Old Chicago";
+        String streetAddress = "11460 NE Evergreen Pkwy";
+        String location = "Hillsboro, OR 97006";
+
+        System.out.println(restaurantName);
+        System.out.println(streetAddress);
+        System.out.println(location);
+    }
+}
